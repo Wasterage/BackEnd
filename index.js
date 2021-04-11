a57caa07a42c12b9b3b73c24c5cd71ccaafd41fa
Abhi Jain <devabhixda@gmail.com>
2021-04-11 11:57:44 +0530

    Add Login/SignUP API
    Signed-off-by: Abhi Jain <devabhixda@gmail.com>

--- index.js
+++ index.js
@@ -54,3 +54,70 @@ app.put(bin+'/:Bin_ID',(req,res)=>{
   res.send('Updated Successfully')
   })
 })
+
+//Citizen
+const citizen = "/citizen";
+var salt = bcrypt.genSaltSync(10);
+
+//CREATE 
+app.post(citizen,(req,res)=>{
+  let user = req.body;
+  var hash = bcrypt.hashSync(user["password"], salt);
+  user["password"] = hash;
+  con.query("INSERT INTO Citizen SET ?",user,(err,rows,fields)=>{
+  if(err) throw err;
+  res.send(JSON.stringify(rows))
+  })
+})
+
+//Login
+app.get(citizen+'/:email/:password',(req,res)=>{
+  con.query('SELECT password from Citizen where `email`=?',[req.params.email], (err,rows,fields)=>{
+    if(err) throw err;
+    if(rows.length == 1) {
+      let pass = rows[0]["password"];
+      let out = {
+          "exists" : 1,
+          "valid": bcrypt.compareSync(req.params.password, pass)
+      }
+    } else {
+      let out = {
+          "exists" : 0
+      }
+    }
+    res.send(out);
+  })
+})
+
+//Driver
+const driver = "/driver";
+
+//CREATE 
+app.post(driver,(req,res)=>{
+  let user = req.body;
+  var hash = bcrypt.hashSync(user["password"], salt);
+  user["password"] = hash;
+  con.query("INSERT INTO Driver SET ?",user,(err,rows,fields)=>{
+  if(err) throw err;
+  res.send(JSON.stringify(rows))
+  })
+})
+
+//Login
+app.get(driver+'/:email/:password',(req,res)=>{
+  con.query('SELECT password from Driver where `email`=?',[req.params.email], (err,rows,fields)=>{
+    if(err) throw err;
+    if(rows.length == 1) {
+      let pass = rows[0]["password"];
+      let out = {
+	      "exists" : 1,
+ 	      "valid": bcrypt.compareSync(req.params.password, pass)
+      }
+	  } else {
+      let out = {
+          "exists" : 0
+      }
+    }
+    res.send(out);
+  })
+})
